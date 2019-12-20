1755ed7df4b54a8097db2d2fec6998630a8786e4
Dileep B <dbellamk@thoughtworks.com>
2019-12-21 00:04:04 +0530

    bootstrap cucumber project with monochromata reporting

---
+++ src/test/java/CucumberMVN/Stepdefs2.java
@@ -0,0 +1,13 @@
+package CucumberMVN;
+
+import cucumber.api.java.en.Given;
+
+public class Stepdefs2 {
+    @Given("today is Monday")
+    public void today_is_Monday() {
+        // Write code here that turns the phrase above into concrete actions
+//        throw new cucumber.api.PendingException();
+        System.out.println("today_is_Monday");
+    }
+
+}
